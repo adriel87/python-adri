fcb828f2c5f7db6bcfecccc51a9c8789299f9dbe
adriel <adrielarochaoronoz@gmail.com>
2023-03-31 18:17:17 +0100

    hello and basics types added

---
+++ beginers/hello.py
@@ -0,0 +1,25 @@
+# comentario de una linea
+# otro de una linea
+
+""" mola 
+y esto es un comentario multilinea
+y te dejo esto de chill
+""" 
+print('hola user')
+
+# vamos a declarar una variable
+
+saludete
+
+# ya esta hay la tienes sin definir ni nada pero vamos a imprimirla a ver que pasa
+
+print(saludete)
+
+# ejecutalo y mira ver que pasa
+
+'''
+es posible que te petara verdad
+si declaramos una variable debemos incializarla
+asi que volvamos a arriba he iguala la variable
+'''
+
---
+++ beginers/tipos.py
@@ -0,0 +1,32 @@
+'''
+veamos que tipos de datos tiene Python
+'''
+
+# str 
+
+"hola soy un str"
+
+'''
+y como sabemos que lo de arriba es un tipo de dato str
+pues simple tenemos una funcion que nos dice que tipo de dato es
+es funcion es type() vamos a darle uso junto con print() para ver el tipo de dato anterior
+y ver si decia la verdad
+'''
+
+print(type("hola soy un str") , " ajaaaa")
+
+'''
+tambien tenemos datos de tipo numerico
+'''
+
+print(type(1) , " numero")
+print(type(1.2) , " numero")
+
+'''
+ambos son numero pero uno es un entero y otro del tipo float
+'''
+
+'''
+y para datos booleanos
+'''
+print(type(1 == 1))
